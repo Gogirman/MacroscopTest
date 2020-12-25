1f9b40b3218cbc23ebf6f152c9edeaf908f6ab72
Георгий Шипицин <gogirman111@gmail.com>
2020-12-25 06:29:26 +0500

    Параметр задержки обработки запроса вынесен в конфиг файл

--- Server/Controllers/PalindromController.cs
+++ Server/Controllers/PalindromController.cs
@@ -1,5 +1,6 @@
 ﻿using CoreStandart.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Threading.Tasks;
@@ -12,18 +13,19 @@ namespace Test.Controllers
     {
         private readonly IServicePalindrom _service;
         private readonly ILogger<PalindromController> _logger;
-
-        public PalindromController(IServicePalindrom service, ILogger<PalindromController> logger)
+        private IConfiguration _config;
+        public PalindromController(IServicePalindrom service, ILogger<PalindromController> logger, IConfiguration config)
         {
             _service = service;
             _logger = logger;
+            _config = config;
         }
 
         [HttpGet]
         public async Task<IActionResult> CheckPalindrom([FromQuery(Name = "text")] string text)
         {
             _logger.LogInformation($"Get request: {text} || {DateTime.Now}");
-            await Task.Delay(TimeSpan.FromSeconds(3));
+            await Task.Delay(TimeSpan.FromSeconds(int.Parse(_config["RequestDelay"])));
             return Ok(await _service.CheckPalindrom(text));
         }
     }
